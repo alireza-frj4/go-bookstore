8448754dffc17c824ce3804afa3336e51472b75a
Alireza Faraji <farajialireza78@gamil.com>
2022-05-07 18:31:43 +0430

    adding files V2

---
+++ pkg/routers/bookstore-routs.go
@@ -0,0 +1,15 @@
+package routers
+
+import (
+	"github.com/alireza-frj4/go-bookstore/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.Creatbook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+
+}
